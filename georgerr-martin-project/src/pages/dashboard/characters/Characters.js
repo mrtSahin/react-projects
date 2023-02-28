f38a58edc4eac21158cd0418489ccc0824210d25
mrtSahin <mrtsvs07@gmail.com>
2023-02-28 14:45:08 +0300

    added axios all

--- georgerr-martin-project/src/pages/dashboard/characters/Characters.js
+++ georgerr-martin-project/src/pages/dashboard/characters/Characters.js
@@ -1,22 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import CharacterDetails from './CharacterDetails'
+import axios from 'axios'
 
 function Characters() {
 
 
   // toplam 2138 tane karakter var
-  const characters=useLocation().state
-  console.log(characters)
+  const characters = useLocation().state //BookDetailsdaki Characters sekmesine yönlendiren Link üzerinde state e eklenen değer
+  //console.log(characters)
+  const [charactersName, setCharactersName] = useState([])
 
-  
+  function getCharactersName() {
+    axios.all(characters)// url dizisini .all ile axoisa ekliyor
+      .then(responses => {
+        responses.forEach(element => {// her url için teker teker axios sorgusunda bulunuyoruz
+          axios(element).then(res => setCharactersName(prev => [...prev, { url: res.data.url, name: res.data.name }]))
+        })
+      });
+  }
+
+  useEffect(() => {
+    getCharactersName() // sorguyu sadece mount anında yapıyoruz
+    return () => {
+      setCharactersName([]) // sıkıntı çıkmasına karşın character in içini unmount anında boşaltıyoruz
+    }
+  }, [])
+
+  //console.log(charactersName)
+
+
+
 
   return (
     <div>Characters
-      {characters&& characters.map(characterUrl=>{
-        const characterID=characterUrl.split('/')
-        return <div><Link to="">{characterID[5]}</Link></div> /** books bize kaakterin sadece api sini verdiğinden dolayı link içerisine karakter ismi yazamıyoruz. çözüm lazım */
-
+      {charactersName.map((character) => {
+        //console.log(character.url)
+        return <div key={character.url}><Link to='/characterDetails' state={{url:character.url,name:character.name}}>{character.name}</Link></div>
       })}
     </div>
   )
